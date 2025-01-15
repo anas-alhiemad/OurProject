5d84fb1179528fec20cf03b3dd74389b570b6975
anas <anasalhiemad@gmail.com>
2025-01-15 20:15:58 +0300

    add updateDisplay0

--- app/Services/InviteService/DisplayInvitationService.php
+++ app/Services/InviteService/DisplayInvitationService.php
@@ -2,21 +2,36 @@
 namespace App\Services\InviteService;
 
 use App\Models\Invitation;
+use App\Repositories\UserRepository;
+use App\Repositories\GroupRepository;
 use App\Repositories\InvitationRepository;
 
 class DisplayInvitationService 
 {
 
     protected $invitationRepository;
-    public function __construct(InvitationRepository $invitationRepository)
+    protected $groupRepository;
+    protected $userRepository;
+
+    public function __construct(InvitationRepository $invitationRepository,GroupRepository $groupRepository,UserRepository $userRepository)
     {
         $this->invitationRepository = $invitationRepository;
+        $this->groupRepository = $groupRepository;
+        $this->userRepository = $userRepository;
+    }
+
+
+    public function isOwner($user,$group)
+    { 
+        if ($user->can('create', $group)) 
+                    return true;
+        return false;            
     }
 
     public function index()
     {
     
-        $allInvitations = $this->invitationRepository->getAll();
+        $allInvitations = $this->invitationRepository->getallinvitedUser();
         return response()->json(["Message"=>"all info for Invitations","infoInvitations"=>$allInvitations]);
     }
 
@@ -30,9 +45,18 @@ class DisplayInvitationService
 
     public function GroupSpecific($groupId)
     {
+        $group = $this->groupRepository->getById($groupId);
+           
+        $user =  $this->userRepository->getById(auth()->guard('user')->id()); 
+        $isOwner = $this->isOwner($user,$group);
+        if ($isOwner){
+            
+        
         $allInvitations =  $this->invitationRepository->groupSpecificInvitation($groupId);
 
         return response()->json(["Message"=>" info Your invitations","infoInvitations"=>$allInvitations]);
+        }
+        return response()->json(["Message"=>"You do not have the authority to do this."]);
     }
 
 }
